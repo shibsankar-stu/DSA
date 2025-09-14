9652ea0d72adf02cd38f7e2aef2f081b422809f6
shibsankar-stu <dasshibsankar94732@gmail.com>
2025-09-14 22:58:11 +0530

    p6

--- patterns/p4.js
+++ patterns/p4.js
@@ -3,7 +3,7 @@ let n = 5;
 function pattern(n){
     for(let i = 1; i <= n; i++){
         let str = " "
-        for(let j = 1   ; j <= n - i + 1; j++){
+        for(let j = 1; j <= n - i + 1; j++){
             str += "* "
         }
       console.log(str);
